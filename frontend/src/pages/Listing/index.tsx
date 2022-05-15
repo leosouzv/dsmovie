ddf0ccf364bba69c3c1af0e16abd19f13c2e010f
leosouzv <leogrdss@icloud.com>
2022-05-15 17:48:50 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -31,10 +31,13 @@ function Listing() {
             });
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number) =>{
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
@@ -42,7 +45,6 @@ function Listing() {
                         <MovieCard movie={movie}/>
                     </div>)
                     )}
-                    
                 </div>
             </div>
         </>
